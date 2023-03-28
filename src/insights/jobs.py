2e9cd4c60010c80fcf703a30609cddc14c3bbc2b
Davi Albino <negroalbino_@hotmail.com>
2023-03-27 23:12:16 -0300

    feat: função read implementada

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,22 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
+    pathList = []
+    try:
+        with open(path) as pathFile:
+            newJobs = list(csv.DictReader(pathFile))
+            pathList = newJobs
+            print(pathList)
+    except FileNotFoundError as err:
+        print("Arquivo não encontrado!")
+        print(err)
+    return pathList
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    # raise NotImplementedError
 
 
 def get_unique_job_types(path: str) -> List[str]:
@@ -53,3 +53,7 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
+
+
+if __name__ == "__main__":
+    read("data/nomes.csv")
