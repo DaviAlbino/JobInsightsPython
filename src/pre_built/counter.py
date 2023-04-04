1495937cbd158c480496195e47cd9cd9177944cb
Davi Albino <negroalbino_@hotmail.com>
2023-04-04 00:24:33 -0300

    test: test counter

--- src/pre_built/counter.py
+++ src/pre_built/counter.py
@@ -3,3 +3,8 @@ def count_ocurrences(path: str, word: str) -> int:
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
     return word_count
+
+
+if __name__ == "__main__":
+    countAllPython = count_ocurrences("data/jobs.csv", "Python")
+    countAllJS = count_ocurrences("data/jobs.csv", "Javascript")
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+# Python = 1639
+# JS = 122
+path = "data/jobs.csv"
+wordPython = "Python"
+wordJS = "Javascript"
 
 
 def test_counter():
-    pass
+    python_ocurrences = count_ocurrences(path, wordPython)
+    js_ocurrences = count_ocurrences(path, wordJS)
+    assert python_ocurrences == 1639
+    assert js_ocurrences == 122
